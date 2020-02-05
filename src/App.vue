dfc8fcf5592d72dffc9e3235220f7c663040f0ef
lenaryan <35894595+lenaryan@users.noreply.github.com>
2020-02-05 22:45:54 +0300

    creating a message component mockup

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <div id="app">
-
+    <Message />
   </div>
 </template>
 
 <script>
-
+import Message from '@/components/Message'
 export default {
   name: 'app',
   components: {
-
+    Message
   }
 }
 </script>
---
+++ src/components/Message.vue
@@ -0,0 +1,16 @@
+<template>
+    <section class="message">
+        <img src="http://placekitten.com/100/100" alt="" class="message__pic">
+        <div class="message__text">
+            <p class="message__name">Name</p>
+            <p class="message__title">Title</p>
+            <p class="message__content">Content</p>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
