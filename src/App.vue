88874a429aa2a5c62c8fff5d0107001b72656dec
lenaryan <35894595+lenaryan@users.noreply.github.com>
2020-02-07 00:24:46 +0300

    adding filter

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
 <template>
   <div id="app">
     <MessageList 
-      v-bind:messages="messages"
+      v-bind:messages="filteredMessages"
     />
+    <div class="filter">
+      <p class="filter__title">Фильтры</p>
+      <label class="filter__label">Имя/Название
+          <input type="text" class="filter__input" v-model="filter1">
+      </label>
+      <label class="filter__label">Контент
+          <input type="text" class="filter__input" v-model="filter2">
+      </label>
+    </div>
   </div>
 </template>
 
@@ -17,7 +26,9 @@ export default {
         {id: 2, name: 'Leanne Graham', title: 'lorem', body: 'lorem lorem ipsum lorem ipsum ipsum'},
         {id: 3, name: 'Leanne Graham', title: 'ipsum', body: 'lorem lorem ipsum lorem ipsum ipsum'}
       ],
-      users: []
+      users: [],
+      filter1: '',
+      filter2: ''
     }
   },
   mounted() {
@@ -25,7 +36,7 @@ export default {
     .then(response => response.json())
     .then(json => this.users = json);
 
-    fetch('https://jsonplaceholder.typicode.com/posts')
+    fetch('https://jsonplaceholder.typicode.com/posts?_limit=7')
     .then(response => response.json())
     .then(json => {
       this.messages = json;
@@ -37,6 +48,25 @@ export default {
         })
       });
     });
+  },
+  computed: {
+    filteredMessages() {
+      if (this.filter1 == '' && this.filter2 == '') {
+        return this.messages;
+      }
+      
+      if (this.filter1 != '' && this.filter2 == '') {
+        return this.messages.filter(mes => mes.title.startsWith(this.filter1));
+      }
+
+      if (this.filter1 == '' && this.filter2 != '') {
+        return this.messages.filter(mes => mes.body.startsWith(this.filter2));
+      }
+
+      if (this.filter1 != '' && this.filter2 != '') {
+        return this.messages.filter(mes => mes.title.startsWith(this.filter1) && mes.body.startsWith(this.filter2));
+      }
+    }
   },
   components: {
     MessageList
